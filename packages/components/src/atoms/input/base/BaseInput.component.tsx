57ba38c80ee2be48dd0ba55c02ca44804bc5dd41
Luis Martínez <lumartch@gmail.com>
2026-07-29 19:49:28 -0600

    chre: restructure components package with new BaseButton and BaseInput components, update tsconfig and vite config

---
+++ packages/components/src/atoms/input/base/BaseInput.component.tsx
@@ -0,0 +1,7 @@
+import type { ComponentProps } from "react";
+
+type Props = ComponentProps<'input'>
+
+export const BaseInput = (props: Props) => {
+    return <input {...props}></input>
+}
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'lumartinez',
       fileName: 'lumartinez-components',
     },
